a9d71808ec8a9da8a9063926d66cb00ee15f511d
Erick Passos <erickcassianopassos@gmail.com>
2025-04-07 00:15:56 -0300

    Adds warning about graphit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
       </div>
 
       <div className={styles.projectList}>
-          <ProjectCard title="Graphit" imgFront={graphitImg1} imgBack={graphitImg2} description="A simple, lightweight web-based graph editor." tryItLink="https://erickcpassos.github.io/graphit/"/>
+          <ProjectCard title="Graphit" imgFront={graphitImg1} imgBack={graphitImg2} description="A simple, lightweight web-based graph editor. (desktop recommended)" tryItLink="https://erickcpassos.github.io/graphit/"/>
           <ProjectCard title="Termo Solver" imgFront={wordleImg1} imgBack={wordleImg2} description="A CLI assistant to win in Wordle-like games (such as the Brazilian 'Termo')." tryItLink="https://github.com/erickcpassos/termo-solver"/>
           <ProjectCard title="Lunetta" imgFront={lunettaImg1} imgBack={lunettaImg2} description="A mobile app to connect low-income students with educational opportunities." tryItLink="https://www.figma.com/proto/vCajqHM9aGAHz2IRQxfo5n/OBT?node-id=570-1479&t=ox1tHIwfxJ4V0KeP-1"/>
 
@@ -72,6 +72,7 @@ export default function Home() {
         <SkillCard title="Arduino"/>
         <SkillCard title="Data Structures & Algorithms"/>
         <SkillCard title="NI Multisim"/>
+        <SkillCard title="KiCad"/>
       </div>
 
 
