f4337a48b2d4b7dc2fee76e86cc68ad5dac2305a
Erick Passos <erickcassianopassos@gmail.com>
2025-05-16 19:42:04 +0100

    Complete navigation bar and fix footer tag

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import styles from "./footer.module.scss";
 
 export default function Footer() {
   return (
-    <nav className={styles.footer}>
+    <footer className={styles.footer}>
         <p className={styles.text}>Made by Erick Passos (@erickcpassos)</p>       
-    </nav>
+    </footer>
   );
 }
--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,30 +1,58 @@
+"use client"
 import Image from "next/image";
 import styles from "./navbar.module.scss";
 
 import navbarMenu from "../../../../public/navbarmenu.svg";
+import navbarClose from "../../../../public/navbarClose.svg";
 import Link from "next/link";
-
-export interface NavbarProps {
-    isShown: boolean;
-}
+import { useState } from "react";
 
 export default function Navbar() {
+
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const closeNavbar = () => { setIsNavbarOpen(false) };
+
   return (
-    <nav className={styles.navbar}>
-        <p className={styles.logo}>Hi! I'm <span>Erick</span>.</p>       
-        <Image className={styles.icon} priority src={navbarMenu} alt="Navbar Menu"/>
+    <>
+      <div className={`${styles.navbarBackdrop} ${!isNavbarOpen ? styles.closed : ''}`}></div>
+
+      <nav className={styles.navbar}>
+
+        <p className={styles.logo}>Hi! I'm <span>Erick</span>.</p>
+
+        <Image className={styles.icon} priority src={!isNavbarOpen ? navbarMenu : navbarClose} alt="Navbar Menu" onClick={() => setIsNavbarOpen(!isNavbarOpen)} />
+        
         <div className={styles.options}>
           <Link href='/' legacyBehavior>
-            <a href="/">About</a>
+            <a href="/" className={styles.dropdownlink}>About</a>
           </Link>
           <Link href='/projects' legacyBehavior>
-            <a href="/projects">Projects</a>
+            <a href="/projects" className={styles.dropdownlink}>Projects</a>
           </Link>
 
           <Link href='/#skills' legacyBehavior>
-            <a href="/#skills">Skills</a>
+            <a href="/#skills" className={styles.dropdownlink}>Skills</a>
           </Link>
         </div>
-    </nav>
+
+
+
+        <div className={`${styles.dropdown} ${!isNavbarOpen ? styles.closed : ''}`}>
+          <Link href='/' legacyBehavior>
+            <a href="/" onClick={closeNavbar} className={styles.dropdownlink}>About</a>
+          </Link>
+
+          <Link href='/projects' legacyBehavior>
+            <a href="/projects" onClick={closeNavbar} className={styles.dropdownlink}>Projects</a>
+          </Link>
+
+          <Link href='/#skills' legacyBehavior>
+            <a href="/#skills" onClick={closeNavbar} className={styles.dropdownlink}>Skills</a>
+          </Link>
+        </div>
+      </nav>
+
+    </>
   );
 }
